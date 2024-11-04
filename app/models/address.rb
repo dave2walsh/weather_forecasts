cd1612d2d223fec171d24ee43b6a48c9d9128ed2
Dave Walsh <dave2walsh@yahoo.ca>
2024-11-04 18:13:02 -0500

    Add current_forecast method to get the current forecast for an address.

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,8 @@ class Address < ApplicationRecord
   validates :city, presence: true, length: {in: 2..49}
   validates :state, presence: true, length: {in: 2..49}
   validates :zip_code, presence: true, length: {in: 5..9}
+
+  def current_forecast
+    forecasts.find_by(kind: "current")
+  end
 end
