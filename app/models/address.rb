896bf56ca574f7c6e987e5d777da2e12e220e9f9
Dave Walsh <dave2walsh@yahoo.ca>
2024-11-04 16:47:23 -0500

    Add some length and presence validations.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,7 @@
 class Address < ApplicationRecord
+  has_many :forecasts
+
+  validates :city, presence: true, length: {in: 2..49}
+  validates :state, presence: true, length: {in: 2..49}
+  validates :zip_code, presence: true, length: {in: 5..9}
 end
