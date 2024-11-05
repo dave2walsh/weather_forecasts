6cb747528ec893274ee3fec61ac28281afba19f6
Dave Walsh <dave2walsh@yahoo.ca>
2024-11-04 19:16:59 -0500

    Add specs for the one_day_forecast and seven_day_forecast methods.

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -79,5 +79,69 @@ RSpec.describe Address, type: :model do
       specify { expect(address.valid?).to be false }
     end
 
+    context "with repeated address" do
+      let(:city) { "Redvillelal" }
+      let(:state) { "California" }
+      let(:zip_code) { "12348" }
+      let!(:address1) { Address.create(city: city, state: state, zip_code: zip_code) }
+      let(:address2) { Address.build(city: city, state: state, zip_code: zip_code) }
+      specify { expect(address2.valid?).to be false }
+    end
+
+    context "with repeated city and state" do
+      let(:city) { "Redvillelal" }
+      let(:state) { "California" }
+      let(:zip_code) { "12348" }
+      let!(:address1) { Address.create(city: city, state: state, zip_code: zip_code) }
+      let(:address2) { Address.build(city: city, state: state, zip_code: "98765") }
+      specify { expect(address2.valid?).to be true }
+    end
+  end
+
+  describe "getting the forecasts for an address" do
+    let(:kind) { "current" }
+    let(:city) { "Largo" }
+    let(:state) { "Tennessee" }
+    let(:zip_code) { "34567" }
+    let(:current_temp) { 70 }
+    let(:high_temp) { 88 }
+    let(:low_temp) { 68 }
+    let!(:address) { Address.create(city: city, state: state, zip_code: zip_code) }
+
+    context "current forecast" do
+      let!(:forecast) { address.forecasts.create(kind: kind, current_temp: current_temp, high_temp: high_temp, low_temp: low_temp) }
+      it "returns the current forecast" do
+        current_forecast = address.current_forecast
+        expect(current_forecast.current_temp).to eq(current_temp)
+        expect(current_forecast.high_temp).to eq(high_temp)
+        expect(current_forecast.low_temp).to eq(low_temp)
+      end
+    end
+
+    context "one_day forecast" do
+      let(:kind) { "one_day" }
+      let(:high_temp) { 102 }
+      let(:low_temp) { 75 }
+      let!(:forecast) { address.forecasts.create(kind: kind, current_temp: current_temp, high_temp: high_temp, low_temp: low_temp) }
+      it "returns the one_day forecast" do
+        one_day_forecast = address.one_day_forecast
+        expect(one_day_forecast.current_temp).to eq(current_temp)
+        expect(one_day_forecast.high_temp).to eq(high_temp)
+        expect(one_day_forecast.low_temp).to eq(low_temp)
+      end
+    end
+
+    context "seven_day forecast" do
+      let(:kind) { "seven_day" }
+      let(:high_temp) { 59 }
+      let(:low_temp) { 38 }
+      let!(:forecast) { address.forecasts.create(kind: kind, current_temp: current_temp, high_temp: high_temp, low_temp: low_temp) }
+      it "returns the seven_day forecast" do
+        seven_day_forecast = address.seven_day_forecast
+        expect(seven_day_forecast.current_temp).to eq(current_temp)
+        expect(seven_day_forecast.high_temp).to eq(high_temp)
+        expect(seven_day_forecast.low_temp).to eq(low_temp)
+      end
+    end
   end
 end
