f1f561a4dfd649cf65146e213ba9a6fcf9fb1bcf
Dave Walsh <dave2walsh@yahoo.ca>
2024-11-04 19:03:19 -0500

    Add a uniqueness constraint for the city, state and zip_code attributes combined.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ApplicationRecord
   has_many :forecasts
 
-  validates :city, presence: true, length: {in: 2..49}
+  validates :city, presence: true, length: {in: 2..49}, uniqueness: { scope: [:state, :zip_code] }
   validates :state, presence: true, length: {in: 2..49}
   validates :zip_code, presence: true, length: {in: 5..9}
 
