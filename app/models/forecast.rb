5be63ee182acda9ecb4aece2f2766133ca9dfcab
Dave Walsh <dave2walsh@yahoo.ca>
2024-11-04 19:01:32 -0500

    Add validations for Forecast.

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,3 +1,6 @@
 class Forecast < ApplicationRecord
   belongs_to :address
+
+  validates :kind, presence: true, inclusion: {in: %(current, one_day, seven_day)}
+  validates :current_temp, :high_temp, :low_temp, numericality: { only_integer: true, in: -100..199 }
 end
