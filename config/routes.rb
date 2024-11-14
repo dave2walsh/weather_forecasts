43b5c732d6e410476979bf8aafe9db31bc9fb6e3
Dave Walsh <dave2walsh@yahoo.ca>
2024-11-14 16:15:13 -0500

    Configure root path.

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "addresses#index"
 end
