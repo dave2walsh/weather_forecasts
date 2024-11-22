c44eda28f3036268a7cb206111a28dd3ddec059e
Dave Walsh <dave2walsh@yahoo.ca>
2024-11-22 15:08:07 -0500

    Add error message to kind inclusion validation, and use Rails config constant.

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,6 +1,6 @@
 class Forecast < ApplicationRecord
   belongs_to :address
 
-  validates :kind, presence: true, inclusion: {in: %(current, one_day)}
+  validates :kind, presence: true, inclusion: {in: Rails.configuration.forecast_types_options.values, message: "%{value} is not a valid type"}
   validates :current_temp, :high_temp, :low_temp, numericality: { only_integer: true, in: -100..199 }
 end
