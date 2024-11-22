76019e13d1e0f3f88aea438c89457c550754d9d4
Dave Walsh <dave2walsh@yahoo.ca>
2024-11-22 15:10:58 -0500

    Use Shoulda Matchers to clean up spec code.

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Forecast, type: :model do
     let(:state) { "Tennessee" }
     let(:zip_code) { "34567" }
     let(:address) { Address.create(city: city, state: state, zip_code: zip_code) }
+    let(:temperature_range) { Range.new(-100, 199) }
 
     subject { address.forecasts.build(kind: kind, current_temp: 66, high_temp: 77, low_temp: 55) }
 
@@ -15,12 +16,12 @@ RSpec.describe Forecast, type: :model do
       specify { expect(forecast.valid?).to be true }
     end
 
-    it { should validate_inclusion_of(:kind).in_array(%w[current, one_day]) }
+    it { should validate_inclusion_of(:kind).in_array(%w[current, one_day]).with_message("Shoulda::Matchers::ExampleClass is not a valid type") }
 
-    it { should validate_numericality_of(:current_temp).is_in(-100..199).only_integer }
+    it { should validate_numericality_of(:current_temp).is_in(temperature_range).only_integer }
 
-    it { should validate_numericality_of(:high_temp).is_in(-100..199).only_integer }
+    it { should validate_numericality_of(:high_temp).is_in(temperature_range).only_integer }
 
-    it { should validate_numericality_of(:low_temp).is_in(-100..199).only_integer }
+    it { should validate_numericality_of(:low_temp).is_in(temperature_range).only_integer }
   end
 end
